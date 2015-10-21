54e52fc2f36712b2bbeff5c1442e3fbce8f28cce
David Lee <david7lee@gmail.com>
2015-10-21 17:30:35 -0400

    implemented working albeit messy/ugly chatterbox client

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,18 @@
 var app = {
 
-  message: {},
+  cache: {},
+  user: {
+    username: '',
+    message: {},
+    friends: {},
+    room: 'lobby'
+  },
+
+  rooms: {},
 
   init: function () {
+    this.user.username = window.location.search.match(/\w+$/gi).join('');
+    setInterval(this.fetch, 500);
   },
 
   send: function (message) {
@@ -19,7 +29,7 @@ var app = {
       contentType: 'application/json',
       //set success: to return on delivery
       success: function () {
-        return true;
+        console.log('message was sent!');
       },
       //set error: to return vague message if it goes to hell.
       error: function () {
@@ -28,27 +38,77 @@ var app = {
     });
   },
 
+
   fetch: function() {
-      //Refactor app.send to make an Ajax request
+    //Refactor app.send to make an Ajax request
+
     $.ajax({
       //set url: to chatterbox on PARSE
-      url: 'https://api.parse.com/1/classes/chatterbox',
+      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1',
       //set type: to 'POST'
       type: 'GET',
-      //set data: to JSON stringified message variable
-      data: JSON.stringify(message),
       //set content/type: to application/json
       contentType: 'application/json',
       //set success: to return on delivery
-      success: function (message) {
-        this.message = message;
-        return true;
+      success: function (data) {
+        app.processMessages(data);
       },
       //set error: to return vague message if it goes to hell.
       error: function () {
-        console.log('Error sending message!');
+        console.log('Error fetching message!');
       }
     });
-    
   },
+
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+
+  addMessage: function(message) {
+    if (message.username in this.user.friends){
+      $('.chatroom').append('<li class=\"username\"><b>' + message.username + '</b></li>');
+
+    } else {
+        $('.chatroom').append('<li class=\"username\">' + message.username + '</li>').on('click', '.username', function (){
+        console.log('friend added!');
+        app.addFriend(message.username);
+      });
+    }
+    $('.chatroom').append('<li class=\"message\">' + message.text + '</li>');
+
+    // Modify addMessages
+  },
+
+  processMessages: function(data) {
+    if( data.results[0].objectId !== this.cache.objectId && data.results[0].roomname === this.user.room){
+      this.cache.objectId = data.results[0].objectId;
+      this.addMessage(data.results[0]);
+    }
+  },
+
+  addRoom: function(roomname) {
+    $('#roomSelect').append('<option value=\"' + roomname + '\">' + roomname + '</option>');
+    this.rooms[roomname] = true;
+  },
+
+  addFriend: function(friend) {
+    this.user.friends[friend] = 'bff';
+  },
+
+  handleSubmit: function(text){
+    this.user.message.username = this.user.username;
+    this.user.message.text = text;
+    this.user.message.roomname = this.user.room;
+
+    this.send(this.user.message);
+    console.log('message was sent');
+  }
 };
+
+app.init();
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -60,6 +60,7 @@ describe('chatterbox', function() {
       it('should submit a GET request via $.ajax', function(done){
         app.fetch();
         expect($.ajax.calledOnce).to.be.true;
+        console.warn(app);
         ajaxUrl = typeof $.ajax.args[0][0] === 'string' ? $.ajax.args[0][0] : $.ajax.args[0][0].url;
         expect(ajaxUrl).to.equal(app.server);
         done();
@@ -119,7 +120,7 @@ describe('chatterbox', function() {
 
         app.init();
 
-        $('#send .submit').trigger('submit');
+        $('.submit').trigger('submit');
         expect(app.handleSubmit.calledOnce).to.be.true;
 
         app.handleSubmit.restore();
